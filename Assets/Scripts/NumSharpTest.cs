b952eecd77a0f2a67a93307c39dc76cb9dd4358c
brettjanz <brettjanz1@gmail.com>
2022-03-15 21:03:39 -0700

    Got sinusoid sonification working with a basic demo test object included in the scene

--- Assets/Scripts/NumSharpTest.cs
+++ Assets/Scripts/NumSharpTest.cs
@@ -5,13 +5,49 @@ using NumSharp;
 
 public class NumSharpTest : MonoBehaviour
 {
+    // Frequencies for different notes
+    float c5_freq = 523f;
+    float e5_freq = 659f;
+    float f5_freq = 698f;
+    float g5_freq = 784f;
+    float a5_freq = 880f;
+    float b5_freq = 988f;
+    float c6_freq = 1047f;
+    float d6_freq = 1175f;
+    float e6_freq = 1319f;
+
     // Start is called before the first frame update
     void Start()
     {
-        var test = np.arange(12);
-        test += 2;
+        // Convert frequencies to numpy arrays
+        NDArray c5_data = Sinusoids.CreateSinusoid(c5_freq);
+        NDArray e5_data = Sinusoids.CreateSinusoid(e5_freq);
+        NDArray f5_data = Sinusoids.CreateSinusoid(f5_freq);
+        NDArray g5_data = Sinusoids.CreateSinusoid(g5_freq);
+        NDArray a5_data = Sinusoids.CreateSinusoid(a5_freq);
+        NDArray b5_data = Sinusoids.CreateSinusoid(b5_freq);
+        NDArray c6_data = Sinusoids.CreateSinusoid(c6_freq);
+        NDArray d6_data = Sinusoids.CreateSinusoid(d6_freq);
+        NDArray e6_data = Sinusoids.CreateSinusoid(e6_freq);
 
-        Debug.Log(test);
+        // Add individual notes to form chords
+        NDArray c_chord = c5_data + e5_data + g5_data;
+        NDArray g_chord = g5_data + b5_data + d6_data;
+        NDArray aM_chord = a5_data + c6_data + e6_data;
+        NDArray f_chord = f5_data + a5_data + c6_data;
+
+        // Concatenate the chords into a (ugly sounding) chord progression
+        NDArray melody = np.hstack(c_chord, g_chord, aM_chord, f_chord);
+
+        // Convert this numpy array to a 4 second audio clip unity can use
+        AudioClip audioClip = Sinusoids.ToAudioClip(melody, "My Melody", 4);
+        
+        // Set the audio source's clip to the clip we just generated
+        AudioSource audioSource = GetComponent<AudioSource>();
+        audioSource.clip = audioClip;
+
+        // Play the clip
+        audioSource.Play();
     }
 
     // Update is called once per frame
---
+++ Assets/Scripts/Sinusoids.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using NumSharp;
+
+public static class Sinusoids
+{
+
+    // Creates a sinusoid and returns a numpy array containing the data
+    public static NDArray CreateSinusoid(float frequency = 440f, float duration = 1f, float samplingRate = 44100f, float amplitude = 1f, float phase = 0f)
+    {
+        NDArray t = np.linspace(0, duration, (int)(samplingRate * duration));
+        NDArray data = amplitude * np.sin(2 * np.pi * frequency * t + phase);
+        return data;
+    }
+
+    // Converts a numpy array of samples into an AudioClip that a unity AudioSource can use
+    public static AudioClip ToAudioClip(NDArray data, string name = "Sinusoid", float duration = 1f, float samplingRate = 44100f)
+    {
+        // Convert from NDArray to regular float[] array
+        float[] samples = data.ToArray<float>();
+
+        // Use Unity's AudioClip constructor
+        AudioClip audioClip = AudioClip.Create(name, (int)(samplingRate * duration), 1, (int)samplingRate, false);
+
+        // Set the AudioClip's data to the array of samples we created
+        audioClip.SetData(samples, 0);
+
+        return audioClip;
+    }
+}
