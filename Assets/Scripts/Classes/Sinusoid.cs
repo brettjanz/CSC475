1bd9a0ebc5e4f81e326176e276ccff39f1ab438a
brettjanz <brettjanz1@gmail.com>
2022-04-10 21:22:39 -0700

    All basic game mechanics implemented. No balancing or real progression milestones, but should fit the requirements laid out for our expected results.

--- Assets/Scripts/Classes/Sinusoid.cs
+++ Assets/Scripts/Classes/Sinusoid.cs
@@ -6,13 +6,19 @@ public class Sinusoid
 {
     private float amplitude;
     private float frequency;
+    private GameObject row;
+    private float upgradeCost;
 
     public Sinusoid(float amplitude, float frequency)
     {
         this.amplitude = amplitude;
         this.frequency = frequency;
+        this.Row = null;
+        this.UpgradeCost = 100f;
     }
 
     public float Amplitude { get => amplitude; set => amplitude = value; }
     public float Frequency { get => frequency; set => frequency = value; }
+    public GameObject Row { get => row; set => row = value; }
+    public float UpgradeCost { get => upgradeCost; set => upgradeCost = value; }
 }
--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -21,9 +21,13 @@ public class Game : MonoBehaviour
 
     // Incremental
     private float cash;
+    private float sinusoidCost;
 
     // UI
     public TextMeshProUGUI cashDisplay;
+    public GameObject sinusoidDisplay;
+    public GameObject rowPrefab;
+    public TextMeshProUGUI addSinusoidButton;
 
     private void Awake()
     {
@@ -35,6 +39,7 @@ public class Game : MonoBehaviour
     void Start()
     {
         cash = 0;
+        sinusoidCost = 10000f;
         sinusoids = new List<Sinusoid>();
         AddSinusoid(new Sinusoid(1f, 440f));
     }
@@ -50,14 +55,30 @@ public class Game : MonoBehaviour
     public void AddSinusoid(Sinusoid newSinusoid)
     {
         sinusoids.Add(newSinusoid);
-        Debug.Log("Added sinusoid: f=" + newSinusoid.Frequency + " A=" + newSinusoid.Amplitude);
         updateSignal();
     }
 
     public void Button_AddSinusoid()
     {
-        int n = sinusoids.Count;
-        AddSinusoid(new Sinusoid(1f, 440f + (220f * n)));
+        if (cash > sinusoidCost)
+        {
+            sinusoidCost *= 10;
+            int n = sinusoids.Count;
+            AddSinusoid(new Sinusoid(1f, 440f + (220f * n)));
+        }
+    }
+
+    public void upgradeSinusoid(int n)
+    {
+        Sinusoid s = sinusoids[n - 1];
+        if (cash > s.UpgradeCost)
+        {
+            // Cost multiplier = 1 + level * 0.25
+            float costMult = 1 + (s.Amplitude * 0.25f);
+            s.Amplitude += 1;            
+            s.UpgradeCost *= costMult;
+            updateSignal();
+        }
     }
 
     private void updateSignal()
@@ -91,11 +112,58 @@ public class Game : MonoBehaviour
             result += s.Frequency * s.Amplitude;
         }
 
-        return result;
+        return result * sinusoids.Count;
     }
 
     private void updateDisplay()
     {
+        // Update cash
         cashDisplay.text = "Cash: " + cash.ToString("G3");
+
+        // Update sinusoid cost
+        addSinusoidButton.text = "Add Sinusoid: $" + sinusoidCost.ToString("G3");
+        
+        // Update sinusoid rows
+        foreach (Sinusoid s in sinusoids)
+        {
+            // Add a row if there is a new sinusoid
+            if (s.Row == null)
+            {
+                int number = sinusoids.Count;
+                GameObject row = Instantiate(rowPrefab, sinusoidDisplay.transform);
+                TextMeshProUGUI sinusoid = getAttributeText(row, "Sinusoid");
+                sinusoid.text = number.ToString();
+                TextMeshProUGUI frequency = getAttributeText(row, "Frequency");
+                frequency.text = s.Frequency.ToString("G3");
+                TextMeshProUGUI amplitude = getAttributeText(row, "Amplitude");
+                amplitude.text = s.Amplitude.ToString("G3");
+                Button upgrade = row.transform.Find("Upgrade").GetComponent<Button>();
+                upgrade.onClick.AddListener(() => upgradeSinusoid(number));
+                TextMeshProUGUI upgradeText = getAttributeText(row, "Upgrade");
+                upgradeText.text = "Upgrade: $" + s.UpgradeCost;
+
+                s.Row = row;
+            }
+            // Otherwise update the amplitude and upgrade costs
+            else
+            {
+                TextMeshProUGUI amplitude = getAttributeText(s.Row, "Amplitude");
+                amplitude.text = s.Amplitude.ToString("G3");
+                TextMeshProUGUI upgradeText = getAttributeText(s.Row, "Upgrade");
+                upgradeText.text = "Upgrade: $" + s.UpgradeCost.ToString("G3");
+            }            
+        }
+    }
+
+    private TextMeshProUGUI getAttributeText(GameObject row, string attr)
+    {
+        if (attr == "Upgrade")
+        {
+            return row.transform.Find(attr).GetComponentInChildren<TextMeshProUGUI>();
+        }
+        else
+        {
+            return row.transform.Find(attr).GetComponent<TextMeshProUGUI>();
+        }
     }
 }
